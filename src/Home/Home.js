1b643a4fb7951ea0f97682132c85dd65b494ec6c
adelstiti <adelw126@gmail.com>
2020-10-02 10:38:21 +0100

    add Submit form  UI

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,7 +6,14 @@ import Person from '../components/Person/Person';
 import Event from '../components/Event/Event';
 import PinMenu from '../components/PinMenu/PinMenu';
 import Avatar from '../components/Avatar/Avatar';
+import Submit from '../components/Submit/Submit';
 
+import  PersonIcon from '@material-ui/icons/Person';
+import List from '@material-ui/icons/List';
+import  EventIcon from '@material-ui/icons/Event';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import TextFormat from '@material-ui/icons/TextFormat';
+import ClearAll from '@material-ui/icons/ClearAll';
 
 function Home() {
 
@@ -16,11 +23,30 @@ function Home() {
       <h3 className="title-ui">UI</h3>
       <div className="App"> 
         <ul>
-          <li  onClick={() => setstate(0)}  className={state === 0 ? 'hover-menu' : ''} > Pin Menu </li>
-          <li  onClick={() => setstate(1)} className={state === 1 ? 'hover-menu' : ''}  > Person </li>
-          <li onClick={() => setstate(2)} className={state === 2 ? 'hover-menu' : ''}  > Menu Person </li>
-          <li  onClick={() => setstate(3)} className={state === 3 ? 'hover-menu' : ''}  > My Event </li>
-          <li  onClick={() => setstate(4)} className={state === 4 ? 'hover-menu' : ''}  > Avatars </li>
+          <li  onClick={() => setstate(0)}  className={state === 0 ? 'hover-menu' : ''} > 
+            Pin Menu
+            <ClearAll style={{float:'left',marginRight : '3%'}} htmlColor="#2C3E50"/> 
+          </li>
+          <li  onClick={() => setstate(1)} className={state === 1 ? 'hover-menu' : ''} >
+            <PersonIcon style={{float:'left',marginRight : '3%'}} htmlColor="#2C3E50"/> 
+            Person 
+            </li>
+          <li onClick={() => setstate(2)} className={state === 2 ? 'hover-menu' : ''}  > 
+            <List style={{float:'left',marginRight : '3%'}} htmlColor="#2C3E50"/> 
+            Menu Person 
+          </li>
+          <li  onClick={() => setstate(3)} className={state === 3 ? 'hover-menu' : ''}  > 
+            <EventIcon style={{float:'left',marginRight : '3%'}} htmlColor="#2C3E50"/> 
+            My Event 
+          </li>
+          <li  onClick={() => setstate(4)} className={state === 4 ? 'hover-menu' : ''}  > 
+            <AccountCircle style={{float:'left',marginRight : '3%'}} htmlColor="#2C3E50"/> 
+            Avatars 
+          </li>
+          <li  onClick={() => setstate(5)} className={state === 4 ? 'hover-menu' : ''}  > 
+            <TextFormat style={{float:'left',marginRight : '3%'}} htmlColor="#2C3E50"/> 
+            Submit Form 
+          </li>
         </ul>
       </div>
       <div className="my-ui">
@@ -29,6 +55,7 @@ function Home() {
         {state === 2 && <MenuPerson />}
         {state === 3 && <Event />}
         {state === 4 && <Avatar />}
+        {state === 5 && <Submit />}
       </div>
     </>
   );
--- src/components/Submit/Submit.js
+++ src/components/Submit/Submit.js
@@ -1,11 +1,29 @@
-import { Close } from '@material-ui/icons'
 import React from 'react'
+import { Close } from '@material-ui/icons'
+import './Submit.css'
 
 const Submit = () => {
     return (
-        <div>
-            <Close />
-            
+        <div className="submit-card">
+            <div className="header-submit">
+                <Close className="close-submit" fontSize="inherit" />
+                <p className="title-submit">Submit a tool</p>
+            </div>
+            <form className="form-submit">
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text"/>
+                <label htmlFor="link">Link to resource</label>
+                <input id="link" type="text"/>
+                <label htmlFor="category">Category</label>
+                <select id="category">
+                    <option value="volvo">Select a category</option>
+                    <option value="saab">Saab</option>
+                    <option value="fiat">Fiat</option>
+                    <option value="audi">Audi</option>
+                </select>
+
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
