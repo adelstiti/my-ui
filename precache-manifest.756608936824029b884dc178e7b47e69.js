03881516b9f4499594c1a5786c6f3e839f8489c1
Adel Stiti <adelw126@gmail.com>
2020-10-29 13:09:53 +0100

    Updates

---
+++ precache-manifest.756608936824029b884dc178e7b47e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451b49e03c3a244364e85c582c8adcbb",
+    "url": "/my-ui/index.html"
+  },
+  {
+    "revision": "3c9b76d7ada8fd5dbc73",
+    "url": "/my-ui/static/css/main.a3fd74b8.chunk.css"
+  },
+  {
+    "revision": "7e52357d76db93e2888a",
+    "url": "/my-ui/static/js/2.7fae2b75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-ui/static/js/2.7fae2b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9b76d7ada8fd5dbc73",
+    "url": "/my-ui/static/js/main.f743c004.chunk.js"
+  },
+  {
+    "revision": "f1168fa0d88e38bb5552",
+    "url": "/my-ui/static/js/runtime-main.21a3abab.js"
+  }
+]);
